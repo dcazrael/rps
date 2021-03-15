10b8c71ea030b3163b225e6e0cd85089750fa96e
dcazrael <msachdev@live.de>
2021-03-16 02:43:30 +0900

    update tailwind.config

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang='en' class="dark">
+      <Html lang='en'>
         <Head>
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
